05af6543003529a1d4e2a2663ff7aa2587f77051
Sanborn <sanbornh@rogers.com>
2014-05-01 17:45:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ElTorneo::Application.config.secret_key_base = '36c37b4fce432edffa21b18282c056f605a89df33993dde6781bece1749b608a8a13c6d2ace8410ce5d9b4d17da217cc46fc620797f4dc799ec00e78aeea8c0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ElTorneo::Application.config.session_store :cookie_store, key: '_el_torneo_session'
