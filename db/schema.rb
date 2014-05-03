d347ffa0eacf94f650210d6bae0b12751d5c7d6b
Taha Jalil <taha.jalil@gmail.com>
2014-05-03 15:00:47 -0400

    added team model, migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501215533) do
+ActiveRecord::Schema.define(version: 20140503185225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20140501215533) do
   create_table "players", force: true do |t|
     t.string   "name"
     t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "team_name"
+    t.integer  "score"
+    t.string   "player1_name"
+    t.string   "player2_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
