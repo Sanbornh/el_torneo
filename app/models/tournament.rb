22b6f3c8e5f400c96db6ddd3c2391c24a59aba4f
Sanborn <sanbornh@rogers.com>
2014-05-04 11:36:55 -0400

    Refactored some tournament.rb code.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,48 +5,52 @@ class Tournament
 		@teams = teams
 		@team_count = teams.length
 		@number_of_games = @team_count - 1
+		@first_round_games = []
+		determine_num_of_rounds
+		
+		generate_bracket
+	end
 
+	def generate_bracket
+		# Used to choose teams randomly for seeding tourny
+		populate_array
+
+		2.times do 
+			team1, team2 = random_team, random_team
+			game = Game.create! 
+			populate_game(team1, team2, game)
+			@first_round_games << game
+		end
+
+		# for first two games in array, create a game
+		# set the parent id of those two games to new game id
+		# put new game into array?
+		# repeat until array is empty
+	end
+
+	def random_team
+		random = @indices.sample(1)[0]
+		team = @teams[random]
+		@indices.delete(random)
+		return team
+	end
+
+	def populate_array
+		@indices = []
+		0.upto(@team_count - 1) { |x| @indices << x }
+	end
+
+	def determine_num_of_rounds
 		if @team_count >= 9 && @team_count <= 16
 			@rounds = 4
 		elsif @team_count >= 17 && @team_count <= 32
 			@rounds = 5
 		end
-
-		generate_bracket
 	end
 
-	def generate_bracket
-		
-		@first_round_games = []
-
-		# Populate an array to reference @teams
-		@indices = []
-		0.upto(@team_count - 1) do |x|
-			@indices << x
-		end		
-
-		2.times do 
-			random = @indices.sample(1)[0]
-			# binding.pry
-			team1 = @teams[random]
-			@indices.delete(random)
-
-			random = @indices.sample(1)[0]
-			# binding.pry
-			team2 = @teams[random]
-			@indices.delete(random)
-
-			game = Game.create! 
-			join = TeamsGame.create!(game_id: game.id, team_id: team1.id) 
-			join = TeamsGame.create!(game_id: game.id, team_id: team2.id)	
-			@first_round_games << game
-		end
-
-
-		# create two root children
-		# create two children of those children
-		# until depth = rounds
-
+	def populate_game(team1, team2, game)
+		TeamsGame.create!(game_id: game.id, team_id: team1.id) 
+		TeamsGame.create!(game_id: game.id, team_id: team2.id)	
 	end
 
 end
