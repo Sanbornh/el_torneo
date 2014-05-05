252685e61a59629b6d0350579e51c3bdd973bcc4
Sanborn <sanbornh@rogers.com>
2014-05-05 19:42:17 -0400

    Halfway through implementing tournament creation for arbitrary pool size

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,20 +1,33 @@
 class Tournament
 
-	def initialize(name, teams)
-		@name = name
-		@teams_in_tourny = teams.to_a 
+	def initialize(type, teams)
+		# @name = name
+		@type = type
+		@teams = teams.to_a 
 		@team_count = teams.length
 		@rounds = determine_num_of_rounds
+		generate_num_first_round_games
 		generate_tournament_structure
 	end
 
+	def generate_num_first_round_games
+		if @team_count <= 8
+			@num_first_round_games = 4
+		elsif @team_count <= 16
+			@num_first_round_games = 8
+		elsif @team_count <= 32
+			@num_first_round_games = 16
+		end
+	end
+
 	def generate_tournament_structure
+		make_first_round
 		seed_first_round
-		generate_other_rounds
+		make_other_rounds
 	end
 
 	def random_team!
-		@teams_in_tourny.shuffle!.pop
+		@teams.shuffle!.pop
 	end
 
 	def determine_num_of_rounds
@@ -30,25 +43,37 @@ class Tournament
 		TeamsGame.create!(game_id: game.id, team_id: team2.id)	
 	end
 
-	def seed_first_round
-		(@team_count / 2).times do 
-			team1, team2 = random_team!, random_team!
-			game = Game.create! 
-			populate_game(team1, team2, game)
-		end
+	def make_first_round
+		@num_first_round_games.times { Game.create!(type: @type) }
 	end
 
-	def generate_other_rounds
+	def seed_first_round
+		games_in_round = Game.roots.to_a
+		until @teams.empty? do
+			binding.pry
+			games_in_round.each do |game|
+				TeamsGame.create!(game_id: game.id, team_id: random_team!)
+				
+			end
 
-		(@rounds - 1).times do 								# do this x times where x is number of rounds after first round
+			binding.pry
 
-			@previous_round = Game.roots.to_a		# make an array with all of the games from the previous round 
+		end
 
+
+		# (@team_count / 2).times do 
+		# 	team1, team2 = random_team!, random_team!
+		# 	game = Game.create!(type: @type) 
+		# 	populate_game(team1, team2, game)
+		# end
+	end
+
+	def make_other_rounds
+		(@rounds - 1).times do 										# do this x times where x is number of rounds after first round
+			@previous_round = Game.roots.to_a				# make an array with all of the games from the previous round 
 			(@previous_round.length / 2).times do 
-
 				child_games = @previous_round.shift(2)
-				parent_game = Game.create
-
+				parent_game = Game.create!(type: @type)
 				child_games.each do |game|
 					game.parent = parent_game
 					game.save
@@ -56,4 +81,5 @@ class Tournament
 			end
 		end
 	end
+
 end
