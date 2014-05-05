3860489286b70870a99ed47d86e63ab024d9d2ea
Sanborn <sanbornh@rogers.com>
2014-05-04 22:02:44 -0400

    Needs to be tested! But creating a tournament should now generate a full bracket structure

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,25 +4,13 @@ class Tournament
 		@name = name
 		@teams_in_tourny = teams.to_a 
 		@team_count = teams.length
-		determine_num_of_rounds
-		generate_bracket
+		@rounds = determine_num_of_rounds
+		generate_tournament_structure
 	end
 
-	def generate_bracket
+	def generate_tournament_structure
 		seed_first_round
-		@next_round_games = []
-
-		(@first_round_games.length / 2).times do 
-			child_games = @first_round_games.shift(2)
-			parent_game = Game.create
-			child_games.each do |game|
-				game.parent = parent_game
-				game.save
-			end
-
-			@next_round_games << parent_game
-		end
-
+		generate_other_rounds
 		# for first two games in array, create a game
 		# set the parent id of those two games to new game id
 		# put new game into array?
@@ -35,9 +23,9 @@ class Tournament
 
 	def determine_num_of_rounds
 		if @team_count >= 9 && @team_count <= 16
-			@rounds = 4
+			4
 		elsif @team_count <= 32
-			@rounds = 5
+			5
 		end
 	end
 
@@ -48,12 +36,32 @@ class Tournament
 
 	def seed_first_round
 		@first_round_games = [] 
-		2.times do 
+		4.times do 
 			team1, team2 = random_team!, random_team!
 			game = Game.create! 
 			populate_game(team1, team2, game)
-			@first_round_games << game
+			# @first_round_games << game
 		end
 	end
 
+	def generate_other_rounds
+
+		(@rounds - 1).times do 								# do this x times where x is number of rounds after first round
+
+			@previous_round = Game.roots.to_a		# make an array with all of the games from the previous round 
+
+			(@previous_round.length / 2).times do 
+				
+				child_games = @previous_round.shift(2)
+				parent_game = Game.create
+
+				child_games.each do |game|
+					game.parent = parent_game
+					game.save
+				end
+
+			end
+
+		end
+	end
 end
