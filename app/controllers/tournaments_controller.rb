321d658da2fcda7c82d42e4787ccb7e89b4d0b33
Sanborn <sanbornh@rogers.com>
2014-05-08 16:03:12 -0400

    Trees set properly now

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,7 +1,16 @@
 class TournamentsController < ApplicationController
 
 	def show
-		@tree_data = Game.arrange_serializable.as_json.to_json
+		puts params[:id]
+
+		if params[:id] == 'pingpong'
+			@tree_data = PingPong.all.arrange_serializable.as_json.to_json
+			# binding.pry
+		else
+			@tree_data = FuzzBall.all.arrange_serializable.as_json.to_json
+			# binding.pry	
+		end
+		# binding.pry
 	end
 
 end
