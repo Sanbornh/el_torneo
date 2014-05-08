7e20b2bb480d8e685941a6236de20bcd0e8ec7ef
Sanborn <sanbornh@rogers.com>
2014-05-08 13:04:12 -0400

    Updated to display tree properly

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,13 +1,7 @@
 class TournamentsController < ApplicationController
 
 	def show
-		if params[:id] == "pingpong"
-			@type = "PingPong"
-		else 
-			@type = "FuzzBall"
-		end 
-
-		@first_round_games = Game.at_depth(3)
+		
 	end
 
 end
