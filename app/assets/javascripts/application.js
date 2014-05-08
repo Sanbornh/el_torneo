b2ec7775baa7799858c142ce80e6bed930b9a15d
Sanborn <sanbornh@rogers.com>
2014-05-08 15:46:55 -0400

    Tree generates based on actual game data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 //= require_tree .
 
 
-$(document).ready(function() {
-  $('#add-score-first-team').click(function(event){
-    event.preventDefault();
-    var url = $(this).data("foo");
-    $.ajax({
-      url: url,
-      type: 'POST',
-      dataType: 'json',
-    }).done(function(data) {
-      alert(data);
-      $('.ref-scorekeeping-page-scoreboard h1 span').text(data);
-    });
-  });
-});
+// $(document).ready(function() {
+//   $('#add-score-first-team').click(function(event){
+//     event.preventDefault();
+//     var url = $(this).data("foo");
+//     $.ajax({
+//       url: url,
+//       type: 'POST',
+//       dataType: 'json',
+//     }).done(function(data) {
+//       alert(data);
+//       $('.ref-scorekeeping-page-scoreboard h1 span').text(data);
+//     });
+//   });
+// });
